9abf2269c35615710cdd8e15d351b210de68996a
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-01-24 14:39:28 +0900

    Add writer

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,12 @@ use std::str::FromStr;
 use std::error::Error;
 use std::fs;
 use std::fs::File;
-// use byteorder::{LittleEndian};
 use byteorder::{ReadBytesExt, WriteBytesExt, LittleEndian};
 
 const TEXT_BIN_FILE_SIZE_MEAN_RATE: &'static usize = &13;
+const DSX_AMP: i16 = i16::max_value();
+const DFX_AMP: f32 = 10000.;
+const DDX_AMP: f64 = 10000.;
 
 enum DType {
     DSA,
@@ -131,40 +133,70 @@ fn read_ddb<T: Read>(src: &mut T, size: usize) -> Result<Vec<f64>, Box<dyn Error
     reader.read_f64_into::<LittleEndian>(&mut buf)?;
     Ok(buf.iter().map(|x| f64::from(*x)).collect())
 }
-//
-// pub fn write_file(filename: &str, src: Vec<f64>) -> Result<(), Box<dyn Error>> {
-//     let mut f = File::create(filename)?;
-//     let file_size = f.metadata()?.len() as usize;
-//     let dtype = DType::from_filename(filename)?;
-//
-//     match dtype {
-//         DType::DSA |
-//         DType::DFA |
-//         DType::DDA => read_dxa(&mut f, file_size),
-//
-//         DType::DSB => read_dsb(&mut f, file_size),
-//         DType::DFB => read_dfb(&mut f, file_size),
-//         DType::DDB => read_ddb(&mut f, file_size),
-//     }
-// }
-//
-// fn write_dxa<T: Write>(dst: T, src: Vec<f64>, dtype: DType) -> Result<(), Box<dyn Error>> {
-//     let mut writer = BufWriter::new(dst);
-//     src.iter().map(|x| writer.write_fmt()
-//     for result in BufWriter::new(dst).lines() {
-//         let line = result?;
-//         let buf: f64 = line.parse::<f64>()?;
-//         ret.push(buf);
-//     }
-//     Ok(ret)
-// }
 
+pub fn write_file(filename: &str, src: Vec<f64>) -> Result<(), Box<dyn Error>> {
+    let mut f = File::create(filename)?;
+    let dtype = DType::from_filename(filename)?;
 
-fn f64s_to_i16s(src: &Vec<f64>) -> Vec<i16> {
-    let amp = 1 << (16 - 1) - 1;
+    match dtype {
+        DType::DSA => write_dxa(&mut f, f64s_to_i16s(&src, DSX_AMP)),
+        DType::DFA => write_dxa(&mut f, f64s_to_f32s(&src, DFX_AMP)),
+        DType::DDA => write_dxa(&mut f, normalize_f64s(src, DDX_AMP)),
+
+        DType::DSB => write_dsb(&mut f, f64s_to_i16s(&src, DSX_AMP)),
+        DType::DFB => write_dfb(&mut f, f64s_to_f32s(&src, DFX_AMP)),
+        DType::DDB => write_ddb(&mut f, normalize_f64s(src, DDX_AMP)),
+    }
+}
+
+fn write_dxa<T: Write, U: std::fmt::Display>(dst: T, src: Vec<U>) -> Result<(), Box<dyn Error>> {
+    let mut writer = BufWriter::new(dst);
+    for x in src {
+        writeln!(writer, "{}", x)?;
+    }
+    Ok(())
+}
+
+fn write_dsb<T: Write>(dst: T, src: Vec<i16>) -> Result<(), Box<dyn Error>> {
+    let mut writer = BufWriter::new(dst);
+    for x in src {
+        writer.write_i16::<LittleEndian>(x)?;
+    }
+    Ok(())
+}
+
+fn write_dfb<T: Write>(dst: T, src: Vec<f32>) -> Result<(), Box<dyn Error>> {
+    let mut writer = BufWriter::new(dst);
+    for x in src {
+        writer.write_f32::<LittleEndian>(x)?;
+    }
+    Ok(())
+}
+
+fn write_ddb<T: Write>(dst: T, src: Vec<f64>) -> Result<(), Box<dyn Error>> {
+    let mut writer = BufWriter::new(dst);
+    for x in src {
+        writer.write_f64::<LittleEndian>(x)?;
+    }
+    Ok(())
+}
+
+fn normalize_f64s(src: Vec<f64>, amp: f64) -> Vec<f64> {
+    let abs_src: Vec<f64> = src.iter().map(|x| x.clone().abs()).collect();
+    let max = max_f64s(&abs_src);
+    src.iter().map(|x| (x / max * amp)).collect()
+}
+
+fn f64s_to_i16s(src: &Vec<f64>, amp: i16) -> Vec<i16> {
     let abs_src: Vec<f64> = src.iter().map(|x| x.clone().abs()).collect();
     let max = max_f64s(&abs_src);
     src.iter().map(|x| (x / max * amp as f64) as i16).collect()
+}
+
+fn f64s_to_f32s(src: &Vec<f64>, amp: f32) -> Vec<f32> {
+    let abs_src: Vec<f64> = src.iter().map(|x| x.clone().abs()).collect();
+    let max = max_f64s(&abs_src);
+    src.iter().map(|x| (x / max * amp as f64) as f32).collect()
 }
 
 fn max_f64s(src: &Vec<f64>) -> f64 {
@@ -185,11 +217,28 @@ fn min_f32s(src: &Vec<f32>) -> f32 {
 
 #[cfg(test)]
 mod tests {
-    use crate::f64s_to_i16s;
+    use crate::*;
 
     #[test]
     fn test_f64s_to_i16s() {
         let src: Vec<f64> = vec![5., -2., 4., -3.];
-        assert_eq!(f64s_to_i16s(&src), vec![16384, -6553, 13107, -9830]);
+        assert_eq!(f64s_to_i16s(&src, DSX_AMP), vec![32767, -13106, 26213, -19660]);
     }
+
+    #[test]
+    fn test_write_file() {
+        let src: Vec<f64> = vec![5., -2., 4., -3.];
+        write_file("a.DSA", src).unwrap();
+        let src: Vec<f64> = vec![5., -2., 4., -3.];
+        write_file("a.DFA", src).unwrap();
+        let src: Vec<f64> = vec![5., -2., 4., -3.];
+        write_file("a.DDA", src).unwrap();
+        let src: Vec<f64> = vec![5., -2., 4., -3.];
+        write_file("a.DSB", src).unwrap();
+        let src: Vec<f64> = vec![5., -2., 4., -3.];
+        write_file("a.DFB", src).unwrap();
+        let src: Vec<f64> = vec![5., -2., 4., -3.];
+        write_file("a.DDB", src).unwrap();
+    }
+
 }
