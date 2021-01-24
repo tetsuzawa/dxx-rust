a061bebe3a77149175966bff91a1a9ecaa066e9a
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2021-01-24 23:08:50 +0900

    Add overlap-add

--- overlap-add-middle/src/main.rs
+++ overlap-add-middle/src/main.rs
@@ -1,6 +1,6 @@
 extern crate dxx;
 
-use anyhow::Result;
+use anyhow::{Result, Error};
 use structopt::StructOpt;
 use std::path::PathBuf;
 
@@ -36,8 +36,115 @@ struct Opt {
 
 fn main() -> Result<()> {
     let opt = Opt::from_args();
-    println!("{:#?}", opt);
+    overlap_add(opt)
+}
+
+
+fn overlap_add(opt: Opt) -> Result<()> {
+    let subject = match opt.subject.to_str() {
+        Some(s) => s,
+        None => return Err(Error::msg("subject is empty"))
+    };
+    let sound_file = match opt.sound_file.to_str() {
+        Some(s) => s,
+        None => return Err(Error::msg("sound_file is empty"))
+    };
+    let move_width = opt.move_width;
+    let move_velocity = opt.move_velocity;
+    let angle = opt.angle;
+    let output = match opt.output.to_str() {
+        Some(s) => s,
+        None => return Err(Error::msg("output is empty"))
+    };
+
+
+    // サンプリング周波数 [sample/sec]
+    let sampling_freq = 48000;
+    // 移動時間 [sec]
+    let move_time: f64 = move_width as f64 / move_velocity as f64;
+    // 移動時間 [sample]
+    let move_samples: u32 = (move_time * sampling_freq as f64) as u32;
+
+    // 0.1度動くのに必要なサンプル数
+    // [sec]*[sample/sec] / [0.1deg] = [sample/0.1deg]
+    let move_samples_per_deg: u32 = move_samples / move_width;
+
+    // 音データの読み込み
+    let sound = dxx::read_file(sound_file)?;
+
+    let sltf_name = format!("{}/SLTF/SLTF_{}_{}.DDB", subject, 0, "L");
+    let sltf = dxx::read_file(sltf_name.as_str())?;
+
+    for direction in ["c", "cc"].iter() {
+        for lr in ["L", "R"].iter() {
+            let mut move_out: Vec<f64> = vec![0.; (move_samples + sltf.len() as u32 - 1) as usize];
+
+            // 使用する角度の計算
+            let clockwise = *direction == "c";
+            let angles = calc_angles(&move_width, &angle, clockwise);
+
+            for (i, val) in angles.iter().enumerate() {
+                let i = i as u32;
+                // SLTFの読み込み
+                let sltf_name = format!("{}/SLTF/SLTF_{}_{}.DDB", subject, val, lr);
+                let sltf = dxx::read_file(sltf_name.as_str())?;
+
+                // 音データと伝達関数の畳込み
+                let cut_sound = sound[(move_samples_per_deg * i) as usize..(move_samples_per_deg * (i + 1)) as usize].to_vec();
+                let sound_sltf = linear_conv(cut_sound, sltf);
+                // Overlap-Add
+                for (j, v) in sound_sltf.iter().enumerate() {
+                    let j = j as u32;
+                    move_out[(move_samples_per_deg * i + j) as usize] += v
+                }
+            }
+
+            let output_name = format!("{}/move_judge_w{:>03}_mt{:>03}d_{}_{}_{}.DDB", output, move_width, move_velocity, direction, angle, lr);
+            let output_len = move_out.len();
+            dxx::write_file(output_name.as_str(), move_out)?;
+            eprintln!("{}, length={}", output_name, output_len);
+            eprintln!("angles={:?}", angles)
+        };
+    };
     Ok(())
-    // let data = dxx::read_file("sine.DSB")?;
-    // dxx::write_file("sine.DSA", data)
+}
+
+fn calc_angles(move_width: &u32, angle: &u32, clockwise: bool) -> Vec<i32> {
+    let move_width = *move_width as i32;
+    let angle = *angle as i32;
+    let start_angle = if clockwise {
+        angle - move_width / 2
+    } else {
+        angle + move_width / 2 - 1
+    };
+
+    let start_angle = if start_angle < 0 {
+        start_angle + 3600
+    } else {
+        start_angle
+    } % 3600;
+
+    let mut angles = Vec::with_capacity(move_width as usize);
+    for i in 0../Users/tetsu/repo/rust-dxxmove_width {
+        let mut data_angle: i32 = (i % (move_width * 2)) as i32;
+        if data_angle > move_width {
+            data_angle = move_width * 2 - data_angle
+        }
+        if !clockwise {
+            data_angle = -1 * data_angle
+        }
+        angles.push((start_angle as i32 + data_angle) % 3600);
+    }
+    angles
+}
+
+fn linear_conv(x: Vec<f64>, y: Vec<f64>) -> Vec<f64> {
+    let conv_len = x.len() + y.len() - 1;
+    let mut ret: Vec<f64> = vec!(0.; conv_len);
+    for p in 0..x.len() {
+        for n in p..y.len() + p {
+            ret[n] += x[p] * y[n - p];
+        }
+    }
+    ret
 }
